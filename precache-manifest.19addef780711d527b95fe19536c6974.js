a256a5ee45dd040df6872eb65fe5ad18096e36b2
“Priyal” <priyal.mittal@gmail.com>
2019-08-26 01:52:50 -0400

    deploy

---
+++ precache-manifest.19addef780711d527b95fe19536c6974.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a7e8282a3951cff298708084b20ed78",
+    "url": "/static/media/ProfilePic.0a7e8282.JPEG"
+  },
+  {
+    "revision": "7f3a0ff2b5db1a56b957",
+    "url": "/static/css/main.cf0359e3.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "f9fc7dc40ef8917c5998",
+    "url": "/static/js/2.f9fc7dc4.chunk.js"
+  },
+  {
+    "revision": "5478e444e24b593a40db549257601d91",
+    "url": "/static/media/xquery.5478e444.png"
+  },
+  {
+    "revision": "0d44456dff5464410d4e67974282e972",
+    "url": "/static/media/postgres.0d44456d.png"
+  },
+  {
+    "revision": "7f3a0ff2b5db1a56b957",
+    "url": "/static/js/main.7f3a0ff2.chunk.js"
+  },
+  {
+    "revision": "e318d18d8a52923f9550eec9f0a603cf",
+    "url": "/static/media/Priyal_Resume_NEU.e318d18d.pdf"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "f9fc7dc40ef8917c5998",
+    "url": "/static/css/2.d2b583e5.chunk.css"
+  },
+  {
+    "revision": "a37862ad10e8792e624654764d9ca5c4",
+    "url": "/index.html"
+  }
+];
